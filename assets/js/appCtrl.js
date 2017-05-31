ae267d2a4be0db6796b71dcf869a77e9815f0e38
DIKSHA JAISWAL <diksha@rytangle.com>
2017-05-31 16:12:50 +0530

    addProduct and addOffer created

--- assets/js/appCtrl.js
+++ assets/js/appCtrl.js
@@ -145,7 +145,7 @@ app.factory("fileReader", function($q, $log) {
                             plotShadow: false,
                             type: 'pie'
                             },
-                           
+
                             title: {
                                 text: scope.title
                             },
@@ -154,7 +154,7 @@ app.factory("fileReader", function($q, $log) {
                              },
                             plotOptions: {
                                           pie: {
-                                                  
+
                                                   allowPointSelect: true,
                                                   cursor: 'pointer',
                                                    dataLabels: {
@@ -287,7 +287,7 @@ app.controller('dashboardCtrl',function($rootScope,$scope,$location){
                         y:175,
                         sliced: false,
                         selected: true
-                    } 
+                    }
                      ]
 
     $scope.sessiondata = [{
@@ -323,14 +323,14 @@ app.controller('dashboardCtrl',function($rootScope,$scope,$location){
             },
                         title: {
                 text: 'Intent'
-            },           
+            },
             yAxis: { title: { text: '' } },
             tooltip: { valueSuffix: ' ' },
             legend: { align: 'center', verticalAlign: 'bottom', borderWidth: 0 },
                plotOptions: {
                 area: {
                   animation: false,
-                   
+
                     fillColor: {
                         linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
                         stops: [
@@ -338,7 +338,7 @@ app.controller('dashboardCtrl',function($rootScope,$scope,$location){
                             [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
                         ]
                     },
-                    
+
                     marker: {
                         radius: 2
                     },
@@ -380,7 +380,7 @@ app.controller('dashboardCtrl',function($rootScope,$scope,$location){
 
 
 
-   
+
 
 
 /*Flow-chart start*/
@@ -388,7 +388,7 @@ var treeData =
   {
     "name": "HI:900",
     "children": [
-      { 
+      {
         "name": "Intent1: 200",
         "children": [
           { "name": "Intent3: 100" },
@@ -510,7 +510,7 @@ function update(source) {
   // Transition to the proper position for the node
   nodeUpdate.transition()
     .duration(duration)
-    .attr("transform", function(d) { 
+    .attr("transform", function(d) {
         return "translate(" + d.y + "," + d.x + ")";
      });
 
@@ -702,104 +702,19 @@ app.controller("addProductCtrl", function($scope, $rootScope){
      $scope.add= function() {
       console.log("hello add function");
       if($scope.items.length<4) {
-       
+
      console.log("hello add function.......");
       $scope.items.push($scope.newitem);
     }
 
       console.log("hello add function");
 
-    
+
 
   }
 };
 });
 
-/*-- TagsCtrl --*/
-app.controller('TagsCtrl', ['$scope', '$timeout', function($scope, $timeout){
-    $scope.inputTags = [];
-    var tags = [];
-    
-       $scope.addTag = function() {
-          console.log('$scope.tagText', $scope.tagText);
-         if ($scope.tagText.length == 0) {
-           return;
-         }
-          if ($scope.tagText.indexOf(',') != -1){
-            var splitTags = $scope.tagText.split(',');
-            tags = splitTags;
-
-          } else {
-            tags.push($scope.tagText);
-          }
-            console.log('wutt', tags.length)
-            for (var i = 0;  i < tags.length; i++) {
-            var tag = $.trim(tags[i]);
-            console.log(tag)
-              $scope.inputTags.push({name: tag});
-            };
-         //this cause flickering of the text, needs to investigate
-         $timeout(function(){$scope.tagText = '';})
-         tags = [];
-
-       }
-    
-       $scope.deleteTag = function(key) {
-         if ($scope.inputTags.length > 0 &&
-           $scope.tagText.length == 0 &&
-           key === undefined) {
-           $scope.inputTags.pop();
-         } else if (key != undefined) {
-           $scope.inputTags.splice(key, 1);
-         }
-       }
-  }]).directive('tagInput', ['$document', function($document){
-    // Runs during compile
-    return {
-      link: function(scope, element, attrs) {
-        scope.inputWidth = 100;
-        console.log('ngModel', attrs.ngModel)
-         // Watch for changes in text field
-         scope.$watch(attrs.ngModel, function(value) {
-           if (value != undefined) {
-
-             var tempEl = $('<span>' + value + '</span>').appendTo('body');
-             var body = $document.find('body').eq(0);
-             var itag = body.append(tempEl);
-             scope.inputWidth = tempEl.width() + 100;
-             tempEl.remove();
-           }
-         });
-
-         element.bind('keydown', function(e) {
-           if (e.which == 9) {
-             e.preventDefault();
-           }
-
-           if (e.which == 8) {
-             scope.$apply(attrs.deleteTag);
-           }
-         });
-
-         element.bind('keyup', function(e) {
-           var key = e.which;
-
-           // Tab or Enter pressed 
-           if (key == 9 || key == 13) {
-             e.preventDefault();
-             scope.$apply(attrs.newTag);
-           }
-         });
-
-         //for pasting
-         element.bind('paste', function(e){
-          console.log('imm getting called',  e)
-          scope.tagText = e.originalEvent.clipboardData.getData('text/plain');
-          scope.$apply(attrs.newTag);
-         });
-      }
-    };
-  }]);
 /* -- faqCtrl : --*/
 
 app.controller("faqCtrl", function($scope,$rootScope, $http){
@@ -989,7 +904,7 @@ app.controller('addAgentCtrl',function($rootScope,$scope,$location,fileReader){
       console.log(agentData);
        console.log(src);
     }
-    
+
     $scope.$on("fileProgress", function(e, progress) {
       $scope.progress = progress.loaded / progress.total;
     });
@@ -1015,4 +930,3 @@ app.controller("webbotsCtrl", function($scope,$rootScope, $http){
   };/* init() */
 
 });
-
